54815d70fc96d9682a7c3cc3e10662d362f83fae
Akuna Natata <nne241823@gmail.com>
2022-10-25 21:19:52 +0300

    Initial Commit

---
+++ contactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contactList
+//
+//  Created by Наталья Никитина on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
